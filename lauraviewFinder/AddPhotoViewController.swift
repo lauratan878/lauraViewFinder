efa7152b633cc06d739519aa4c7d06f0e7b80812
Apple <apple@Apples-MacBook-Pro-41.local>
2019-06-25 13:19:49 -0700

    iteration2

--- lauraviewFinder/AddPhotoViewController.swift
+++ lauraviewFinder/AddPhotoViewController.swift
@@ -14,29 +14,45 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
     var imagePicker = UIImagePickerController()
     
     
-    
     @IBOutlet weak var newImage: UIImageView!
     
-
+    @IBOutlet weak var caption: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
     
-    
-
-    
     @IBAction func accessCamera(_ sender: UIButton) {
         imagePicker.sourceType = .camera
         present(imagePicker, animated : true, completion : nil)
     }
     
-    
     @IBAction func accessAlbums(_ sender: UIButton) {
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated : true, completion : nil)
     }
+    
+    
+    @IBAction func savePhoto(_ sender: UIButton) {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            let photoToSave = Photos(entity: Photos.entity(), insertInto: context)
+            photoToSave.caption = caption.text
+            if let userImage = newImage.image {
+                if let userImageData = userImage.pngData() {
+                    photoToSave.imageData = userImageData
+                }
+            }
+        }
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        navigationController?.popViewController(animated: true)
+        
+
+    }
+    
+   
+    
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
