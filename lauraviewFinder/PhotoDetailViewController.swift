40b16ab1936a54456ad0991a99685f3c78dbed7b
Apple <apple@Apples-MacBook-Pro-41.local>
2019-06-25 13:57:39 -0700

    iterationsomeof3

---
+++ lauraviewFinder/PhotoDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoDetailViewController.swift
+//  lauraviewFinder
+//
+//  Created by Apple on 6/25/19.
+//  Copyright © 2019 Laura. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
